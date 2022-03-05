463d8645be5d62b6aa47ce5cf23a2b1a868951a5
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2022-03-05 17:01:00 +0630

    chore: merge cli config by spreading

--- modules/pika/src/vite/build.js
+++ modules/pika/src/vite/build.js
@@ -1,5 +1,5 @@
 import path from 'path'
-import { build as viteBuild, mergeConfig } from 'vite'
+import { build as viteBuild } from 'vite'
 
 /** @param {import('vite').InlineConfig} config */
 export async function build(config) {
@@ -9,12 +9,21 @@ export async function build(config) {
 
 /** @param {import('vite').InlineConfig} config */
 async function buildClient(config) {
+  config = {
+    ...config,
+    build: {
+      ssrManifest: false,
+      ...config.build,
+    },
+  }
   await viteBuild(config)
 }
 
 /** @param {import('vite').InlineConfig} config */
 async function buildServer(config) {
-  config = mergeConfig(config, {
+  config = {
+    ...config,
+    publicDir: false,
     build: {
       ssr: true,
       rollupOptions: {
@@ -22,8 +31,9 @@ async function buildServer(config) {
           format: 'esm',
         },
       },
+      ...config.build,
     },
-  })
+  }
   await viteBuild(config)
 }
 
@@ -35,15 +45,13 @@ export function buildPlugin(options) {
   return {
     name: 'pika:build',
     apply: 'build',
-    config({ publicDir, build }) {
+    config({ build }) {
       const ssr = !!build?.ssr
 
       return {
-        publicDir: ssr ? false : publicDir,
         build: {
           assetsDir: '_pika',
           outDir: path.join(build?.outDir || 'dist', ssr ? 'server' : 'client'),
-          ssrManifest: !ssr,
           ssr: ssr ? options.ssrEntry : false,
         },
       }
