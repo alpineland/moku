f11860b64dd9420dfedc3c65052d3d1b58f4cc9d
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2022-03-15 18:00:43 +0630

    chore: some vite plugin utils

--- modules/pika/src/vite_plugin/utils.js
+++ modules/pika/src/vite_plugin/utils.js
@@ -21,3 +21,48 @@ export function readTemplate(root, template) {
     throw new Error(`${template} does not exist`)
   }
 }
+
+/**
+ * @param {string} dir
+ * @param {RegExp} exclude
+ * @returns {AsyncIterableIterator<string>}
+ */
+export async function* walk(dir, exclude) {
+  const files = await fs.promises.opendir(dir)
+  for await (const file of files) {
+    if (!exclude.test(file.name)) {
+      const entry = path.join(dir, file.name)
+      if (file.isDirectory()) yield* walk(entry, exclude)
+      else if (file.isFile()) yield entry
+    }
+  }
+}
+
+/** @param {string} dir */
+export function mkdirp(dir) {
+  try {
+    fs.mkdirSync(dir, { recursive: true })
+  } catch (/** @type {any} */ e) {
+    if (e.code === 'EEXIST') return
+    throw e
+  }
+}
+
+/**
+ * @param {import('vite').ResolvedConfig} config
+ * @param {string} entry
+ * @param {string[]} exts `['.js', '.ts']`
+ * @returns {string} absolute entry
+ */
+export function getEntry(config, entry, exts = ['.js', '.ts']) {
+  const { root } = config
+  const absEntry = path.join(root, entry)
+
+  const ext = exts.find((ext) => fs.existsSync(absEntry + ext))
+
+  if (!ext) {
+    throw new Error(`Missing "${entry}" in ${root}`)
+  }
+
+  return absEntry + ext
+}
