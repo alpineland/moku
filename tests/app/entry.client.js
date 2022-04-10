d256f3d44ff70e81402363706d6cb7f09fa0c2b6
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2022-04-10 23:25:56 +0630

    haha! code splitting works now!

--- tests/app/entry.client.js
+++ tests/app/entry.client.js
@@ -1,7 +1,8 @@
 import { createUniApp } from './main.js';
 
+const { app, router } = createUniApp();
+
 /** @type {import('pika').StartClient} */
 export function start({ el }) {
-  const { app, router } = createUniApp();
   router.isReady().then(() => app.mount(el));
 }
